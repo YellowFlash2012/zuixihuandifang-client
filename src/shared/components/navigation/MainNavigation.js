1efffd3bfe844fac7866390927ed95a1cd4f9658
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-07 07:18:51 +0100

    sideDrawer functionality and animation completed

--- src/shared/components/navigation/MainNavigation.js
+++ src/shared/components/navigation/MainNavigation.js
@@ -1,29 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {Link} from "react-router-dom"
 
 import MainHeader from './MainHeader'
 
+import SideDrawer from './SideDrawer'
+
 import "./MainNavigation.css"
+
 import NavLinks from './NavLinks'
 
+import Backdrop from "../UI/Backdrop"
+
 const MainNavigation = (props) => {
+
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
+
     return (
-        <MainHeader>
-            <button className="main-navigation__menu-btn">
-                <span />
-                <span />
-                <span />
-            </button>
+        <>
+            {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
 
-            <h1 className="main-navigation__title">
-                <Link to="/">Your Places</Link>
-            </h1>
+            <SideDrawer
+                show={drawerIsOpen}
+                onClick={closeDrawer}
+            >
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
 
-            <nav>
-                <NavLinks />
-            </nav>
-        </MainHeader>
+            <MainHeader>
+                <button className="main-navigation__menu-btn" onClick={openDrawer}>
+                    <span />
+                    <span />
+                    <span />
+                </button>
+
+                <h1 className="main-navigation__title">
+                    <Link to="/">Your Places</Link>
+                </h1>
+
+                <nav className="main-navigation__header-nav">
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </>
     );
 }
 
