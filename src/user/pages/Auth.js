9b514fd76b7c27d3577ba639d08e8de715153c24
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-14 06:52:40 +0100

    added the fetch functionality to the login form to connect it to the backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -6,8 +6,11 @@ import { useForm } from "../../shared/hooks/form-hook"
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/Util/validators'
 
 import Button from "../../shared/components/FormElements/Button"
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Card from '../../shared/components/UI/Card'
+
+import LoadingSpinner from "../../shared/components/UI/LoadingSpinner"
+import ErrorModal from "../../shared/components/UI/ErrorModal"
 
 import {AuthContext} from "../../shared/context/auth-context"
 
@@ -60,9 +63,42 @@ const Auth = () => {
     const authHandler = async (e) => {
         e.preventDefault();
 
+        setIsLoading(true);
+
         if (isLoginMode) {
-            
+            try {
+                const res = await fetch(
+                    "http://localhost:5000/api/users/login",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            
+                            email: formState.inputs.email.value,
+                            password: formState.inputs.password.value,
+                        }),
+                    }
+                );
+
+                const data = await res.json();
+
+                if (!res.ok) {
+                    throw new Error(data.message);
+                }
+
+                console.log(data);
+                setIsLoading(false);
+                auth.login();
+            } catch (err) {
+                console.log(err);
+                setError(
+                    err.message || "Something went wrong, please try again!"
+                );
+            }
         } else {
+            
             try {
                 const res = await fetch(
                     "http://localhost:5000/api/users/signup",
@@ -81,62 +117,76 @@ const Auth = () => {
 
                 const data = await res.json();
 
+                if (!res.ok) {
+                    throw new Error(data.message)
+                }
+
                 console.log(data);
+                setIsLoading(false);
+                auth.login();
 
             } catch (err) {
                 console.log(err);
+                setError(err.message || "Something went wrong, please try again!");
             }
         }
 
-        
-
         console.log(formState.inputs);
-        auth.login();
+        
         navigate("/")
     };
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
     return (
-        <Card className="authentication">
-            <form className="place-form" onSubmit={authHandler}>
-                {!isLoginMode && (
+        <>
+            <ErrorModal error={error} onClear={errorHandler} />
+            <Card className="authentication">
+                {isLoading && <LoadingSpinner asOverlay />}
+
+                <form className="place-form" onSubmit={authHandler}>
+                    {!isLoginMode && (
+                        <Input
+                            id="name"
+                            element="input"
+                            type="text"
+                            label="Name"
+                            validators={[VALIDATOR_REQUIRE()]}
+                            errorText="Please enter a name."
+                            onInput={inputHandler}
+                        />
+                    )}
                     <Input
-                        id="name"
+                        id="email"
                         element="input"
-                        type="text"
-                        label="Name"
-                        validators={[VALIDATOR_REQUIRE()]}
-                        errorText="Please enter a name."
+                        type="email"
+                        label="Email"
+                        validators={[VALIDATOR_EMAIL()]}
+                        errorText="Please enter a valid email address."
                         onInput={inputHandler}
                     />
-                )}
-                <Input
-                    id="email"
-                    element="input"
-                    type="email"
-                    label="Email"
-                    validators={[VALIDATOR_EMAIL()]}
-                    errorText="Please enter a valid email address."
-                    onInput={inputHandler}
-                />
 
-                <Input
-                    id="password"
-                    element="input"
-                    label="Password"
-                    type="password"
-                    validators={[VALIDATOR_MINLENGTH(13)]}
-                    errorText="Password must be at least 13 characters"
-                    onInput={inputHandler}
-                />
+                    <Input
+                        id="password"
+                        element="input"
+                        label="Password"
+                        type="password"
+                        validators={[VALIDATOR_MINLENGTH(13)]}
+                        errorText="Password must be at least 13 characters"
+                        onInput={inputHandler}
+                    />
 
-                <Button type="submit" disabled={!formState.isValid}>
-                    {isLoginMode ? "LOGIN" : "SIGN UP"}
+                    <Button type="submit" disabled={!formState.isValid}>
+                        {isLoginMode ? "LOGIN" : "SIGN UP"}
+                    </Button>
+                </form>
+                <Button inverse onClick={switchModeHandler}>
+                    {isLoginMode ? "SIGN UP" : "LOGIN"} INSTEAD
                 </Button>
-            </form>
-            <Button inverse onClick={switchModeHandler}>
-                {isLoginMode ? "SIGN UP" : "LOGIN"} INSTEAD
-            </Button>
-        </Card>
+            </Card>
+        </>
     );
 }
 
