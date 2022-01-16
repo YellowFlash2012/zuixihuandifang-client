75d14242ef1cb495d219b9897bcddebf2c69a785
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-16 15:42:49 +0100

    finished setting the file upload component in the signup form followed by a successful testing

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -46,13 +46,18 @@ const Auth = () => {
         if (!isLoginMode) {
             setFormData({
                 ...formState.inputs,
-                name:undefined
+                name: undefined,
+                image:undefined
             }, formState.inputs.email.isValid && formState.inputs.password.isValid)
         } else {
             setFormData({
                 ...formState.inputs,
                 name: {
                     value: '',
+                    isValid: false
+                },
+                image: {
+                    value:null,
                     isValid: false
                 }
             }, false);
@@ -62,6 +67,8 @@ const Auth = () => {
 
     const authHandler = async (e) => {
         e.preventDefault();
+
+        console.log(formState.inputs);
 
         if (isLoginMode) {
             try {
@@ -135,7 +142,7 @@ const Auth = () => {
                         />
                     )}
 
-                    {!isLoginMode && <ImageUpload center id="image" />}
+                    {!isLoginMode && <ImageUpload center id="image" onInput={inputHandler} />}
 
                     <Input
                         id="email"
